1939a51a1d7f3595d4d3f0d94500b2dfd5b97136
NicolasOrozcoMedina <norozcom@unal.edu.co>
2025-02-12 20:40:54 -0500

    Laboratorio Completado

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,138 +1,44 @@
-# flake8: noqa: E501
 """Autograding script."""
 
-import gzip
-import json
 import os
-import pickle
 
 import pandas as pd  # type: ignore
 
-# ------------------------------------------------------------------------------
-MODEL_FILENAME = "files/models/model.pkl.gz"
-MODEL_COMPONENTS = [
-    "OneHotEncoder",
-    "RandomForestClassifier",
-]
-SCORES = [
-    0.785,
-    0.673,
-]
-METRICS = [
-    {
-        "type": "metrics",
-        "dataset": "train",
-        "precision": 0.944,
-        "balanced_accuracy": 0.785,
-        "recall": 0.580,
-        "f1_score": 0.719,
-    },
-    {
-        "type": "metrics",
-        "dataset": "test",
-        "precision": 0.650,
-        "balanced_accuracy": 0.673,
-        "recall": 0.401,
-        "f1_score": 0.498,
-    },
-    {
-        "type": "cm_matrix",
-        "dataset": "train",
-        "true_0": {"predicted_0": 16060, "predicted_1": None},
-        "true_1": {"predicted_0": None, "predicted_1": 2740},
-    },
-    {
-        "type": "cm_matrix",
-        "dataset": "test",
-        "true_0": {"predicted_0": 6670, "predicted_1": None},
-        "true_1": {"predicted_0": None, "predicted_1": 760},
-    },
-]
+from homework import clean_data
 
 
-# ------------------------------------------------------------------------------
-#
-# Internal tests
-#
-def _load_model():
-    """Generic test to load a model"""
-    assert os.path.exists(MODEL_FILENAME)
-    with gzip.open(MODEL_FILENAME, "rb") as file:
-        model = pickle.load(file)
-    assert model is not None
-    return model
+def test_01():
+    """Test homework/clean_data.py"""
 
+    clean_data.main(
+        "files/input/input.txt",
+        "files/output.txt",
+    )
 
-def _test_components(model):
-    """Test components"""
-    assert "GridSearchCV" in str(type(model))
-    current_components = [str(model.estimator[i]) for i in range(len(model.estimator))]
-    for component in MODEL_COMPONENTS:
-        assert any(component in x for x in current_components)
+    if not os.path.exists("files/test.csv"):
+        raise FileNotFoundError("File 'files/test.csv' not found")
 
+    test = pd.read_csv("files/test.csv", index_col=None)
 
-def _load_grading_data():
-    """Load grading data"""
-    with open("files/grading/x_train.pkl", "rb") as file:
-        x_train = pickle.load(file)
+    assert test.loc[0, "key"] == "acdeghinoqruy"
+    assert test.loc[1, "key"] == "acdegilmnoty"
+    assert test.loc[3, "key"] == "acdehioqrsu"
+    assert test.loc[6, "key"] == "acdehoqruy"
+    assert test.loc[12, "key"] == "acdeilmnoty"
+    assert test.loc[16, "key"] == "acdgilnoprstu"
 
-    with open("files/grading/y_train.pkl", "rb") as file:
-        y_train = pickle.load(file)
+    #
+    # Retorna error si la carpeta output/ no existe
+    if not os.path.exists("files/output.txt"):
+        raise FileNotFoundError("File 'files/output.txt' not found")
 
-    with open("files/grading/x_test.pkl", "rb") as file:
-        x_test = pickle.load(file)
+    #
+    # Lee el contenido del archivo output.txt
+    dataframe = pd.read_csv("files/output.txt")
+    count = dataframe.groupby("text").size()
 
-    with open("files/grading/y_test.pkl", "rb") as file:
-        y_test = pickle.load(file)
-
-    return x_train, y_train, x_test, y_test
-
-
-def _test_scores(model, x_train, y_train, x_test, y_test):
-    """Test scores"""
-    assert model.score(x_train, y_train) > SCORES[0]
-    assert model.score(x_test, y_test) > SCORES[1]
-
-
-def _load_metrics():
-    assert os.path.exists("files/output/metrics.json")
-    metrics = []
-    with open("files/output/metrics.json", "r", encoding="utf-8") as file:
-        for line in file:
-            metrics.append(json.loads(line))
-    return metrics
-
-
-def _test_metrics(metrics):
-
-    for index in [0, 1]:
-        assert metrics[index]["type"] == METRICS[index]["type"]
-        assert metrics[index]["dataset"] == METRICS[index]["dataset"]
-        assert metrics[index]["precision"] > METRICS[index]["precision"]
-        assert metrics[index]["balanced_accuracy"] > METRICS[index]["balanced_accuracy"]
-        assert metrics[index]["recall"] > METRICS[index]["recall"]
-        assert metrics[index]["f1_score"] > METRICS[index]["f1_score"]
-
-    for index in [2, 3]:
-        assert metrics[index]["type"] == METRICS[index]["type"]
-        assert metrics[index]["dataset"] == METRICS[index]["dataset"]
-        assert (
-            metrics[index]["true_0"]["predicted_0"]
-            > METRICS[index]["true_0"]["predicted_0"]
-        )
-        assert (
-            metrics[index]["true_1"]["predicted_1"]
-            > METRICS[index]["true_1"]["predicted_1"]
-        )
-
-
-def test_homework():
-    """Tests"""
-
-    model = _load_model()
-    x_train, y_train, x_test, y_test = _load_grading_data()
-    metrics = _load_metrics()
-
-    _test_components(model)
-    _test_scores(model, x_train, y_train, x_test, y_test)
-    _test_metrics(metrics)
+    assert count.loc["AD-HOC QUERIES"] == 3
+    assert count.loc["AGRICULTURAL PRODUCTION"] == 1
+    assert count.loc["AIRLINE COMPANIES"] == 1
+    assert count.loc["AIRLINES"] == 1
+    assert count.loc["ANALYTIC APPLICATIONS"] == 9
